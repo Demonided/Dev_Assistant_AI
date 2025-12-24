4b0677b3cbe6c0e7a74d81d6971dba94967ffdf1
Maks_Shostak <demonided@mail.ru>
2025-12-24 16:23:54 +0300

    update

--- feature/quiz/src/main/java/com/example/tests/presentation/QuizScreen.kt
+++ feature/quiz/src/main/java/com/example/tests/presentation/QuizScreen.kt
@@ -1,6 +1,5 @@
 package com.example.tests.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tests.domain.model.AnswerState
