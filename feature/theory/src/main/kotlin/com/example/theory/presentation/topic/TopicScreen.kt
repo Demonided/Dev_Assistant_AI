5e10b9dcd362deaef2abeebb9be731ecf0430ba4
gkuznetsov <kuznetsov.mdev@gmail.com>
2025-12-24 21:59:28 +0300

    13-topic-screen: add content padding for TopicCards

--- feature/theory/src/main/kotlin/com/example/theory/presentation/topic/TopicScreen.kt
+++ feature/theory/src/main/kotlin/com/example/theory/presentation/topic/TopicScreen.kt
@@ -1,11 +1,11 @@
 package com.example.theory.presentation.topic
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -35,11 +35,11 @@ fun TopicScreen(
 
         LazyColumn(
             modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(10.dp)
+            contentPadding = PaddingValues(10.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             items(viewModel.state.value.topics) { topic ->
                 TopicCard(
-                    modifier = Modifier.padding(10.dp),
                     topicData = topic,
                     onClick = {}
                 )
