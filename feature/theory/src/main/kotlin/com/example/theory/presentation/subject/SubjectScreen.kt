226c0e9d36f58a1dd20c48daa1f595451c69f1d6
gkuznetsov <kuznetsov.mdev@gmail.com>
2026-01-17 17:43:15 +0300

    20-article-screen: apply common header component for SubjectScreen

--- feature/theory/src/main/kotlin/com/example/theory/presentation/subject/SubjectScreen.kt
+++ feature/theory/src/main/kotlin/com/example/theory/presentation/subject/SubjectScreen.kt
@@ -12,9 +12,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.theory.presentation.subject.ui.components.SubjectHeaderComponent
+import com.example.theory.R
+import com.example.theory.common.ui.components.DevAssistantHeader
+import com.example.theory.presentation.subject.ui.components.AppSearchField
 import com.example.theory.presentation.subject.ui.components.SubjectsComponent
 import com.example.ui_theme.ui.theme.DevAssistantAITheme
+import com.example.ui_theme.ui.theme.DevAssistantGradient
 import com.example.ui_theme.ui.theme.DevAssistantTheme
 
 @Composable
@@ -31,7 +34,17 @@ fun SubjectScreen(
             .fillMaxSize()
             .background(DevAssistantTheme.colors.appBackground)
     ) {
-        SubjectHeaderComponent {}
+        DevAssistantHeader(
+            showBackButton = false,
+            title = R.string.select_topic,
+            subTitle = R.string.start_preparation,
+            background = DevAssistantGradient.primary()
+        ) {
+            AppSearchField(
+                value = "",
+                { }
+            )
+        }
         SubjectsComponent(subjects = state, onClick = onSubjectClick)
 
     }
