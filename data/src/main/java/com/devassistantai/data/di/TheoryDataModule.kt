9035364a03eedb93ce196faa4a7a279868a99d77
gkuznetsov <kuznetsov.mdev@gmail.com>
2026-02-12 19:42:57 +0300

    24-article-business-layer: add data layer for Article feature

--- data/src/main/java/com/devassistantai/data/di/TheoryDataModule.kt
+++ data/src/main/java/com/devassistantai/data/di/TheoryDataModule.kt
@@ -1,7 +1,9 @@
 package com.devassistantai.data.di
 
+import com.devassistantai.data.repository.mock.MockArticleRepositoryImpl
 import com.devassistantai.data.repository.mock.MockSubjectRepositoryImpl
 import com.devassistantai.data.repository.mock.MockTopicRepositoryImpl
+import com.devassistantai.domain.repository.ArticleRepositoryApi
 import com.devassistantai.domain.repository.TheoryRepositoryApi
 import com.devassistantai.domain.repository.TopicRepositoryApi
 import dagger.Binds
@@ -18,4 +20,7 @@ interface TheoryDataModule {
 
     @Binds
     fun bindMockTopicRepository(impl: MockTopicRepositoryImpl): TopicRepositoryApi
+
+    @Binds
+    fun bindMockArticleRepository(impl: MockArticleRepositoryImpl): ArticleRepositoryApi
 }
