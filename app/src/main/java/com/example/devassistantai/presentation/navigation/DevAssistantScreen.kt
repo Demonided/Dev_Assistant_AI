98f21654e5f63092411d4b2fb3fa13bf38fcc106
gkuznetsov <kuznetsov.mdev@gmail.com>
2026-02-19 18:27:03 +0300

    30-nav3: create instances of NavigationState and Navigator

--- app/src/main/java/com/example/devassistantai/presentation/navigation/DevAssistantScreen.kt
+++ app/src/main/java/com/example/devassistantai/presentation/navigation/DevAssistantScreen.kt
@@ -3,9 +3,17 @@ package com.example.devassistantai.presentation.navigation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
+import com.example.devassistantai.presentation.navigation.nav3.Navigator
+import com.example.devassistantai.presentation.navigation.nav3.rememberNavigationState
+import com.example.devassistantai.presentation.navigation.nav3.routes.AccountNavKey
+import com.example.devassistantai.presentation.navigation.nav3.routes.ChatAINavKey
+import com.example.devassistantai.presentation.navigation.nav3.routes.QuizNavKey
+import com.example.devassistantai.presentation.navigation.nav3.routes.SettingsNavKey
+import com.example.devassistantai.presentation.navigation.nav3.routes.theory.SubjectNavKey
 import com.example.devassistantai.presentation.navigation.navbar.CustomBottomBar
 
 @Composable
@@ -14,6 +22,12 @@ fun DevAssistantScreen(
     modifier: Modifier = Modifier
 ) {
     val navController = rememberNavController()
+
+    val navigationState = rememberNavigationState(
+        startRoute = SubjectNavKey,
+        topLevelRoutes = setOf(SubjectNavKey, QuizNavKey, ChatAINavKey, AccountNavKey, SettingsNavKey)
+    )
+    val navigator = remember { Navigator(navigationState) }
 
     Scaffold(
         bottomBar = {
